39a40f4d71fa8e7920012d23bd0b2c33f50a1ddf
Anh Truong <anh.truong.07@cnu.edu>
2016-07-27 19:09:44 -0400

    seeded

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var express = require("express");
+var hbs     = require("express-handlebars");
+var parser = require("body-parser");
+var Restauraunt = mongoose.model("Restauraunt"
+var MenuItem = mongoose.model("MenuItem");
+
+var app = express();
+
+app.set("port", process.env.PORT || 1337);
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
+}));
